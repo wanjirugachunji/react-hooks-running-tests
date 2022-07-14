b0ee0d52c738f70f3a7f84bb1ecc2a314bc31fce
wanjirugachunji <wanjirugachunji@gmail.com>
2022-07-14 12:26:49 +0300

    not yet done

--- src/components/Article.js
+++ src/components/Article.js
@@ -6,6 +6,7 @@ function Article() {
       {/* please pass this test */}
     </div>
   );
+    return <div>please pass this test</div>;
 }
 
 export default Article;
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,11 +2,15 @@ import React from "react";
 
 function Header(props) {
       return (
+    return (
         <h1>
           {/* uncomment the line below to pass the test */}
           {/* hello from the Header! */}
+            {/* uncomment the line below to pass the test */}
+            hello from the Header!
         </h1>
       )
+    );
 }
 
 export default Header;
